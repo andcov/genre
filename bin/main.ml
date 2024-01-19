e4ad077d6771eec5dc4d3c928c9bdfb7370f26c0
andcov <andreicovaci123@gmail.com>
2024-01-19 16:02:27 +0200

    Optimize chars to strings

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,8 @@
 let () =
-  let r = Genre.Parser.parse {| [^i*&2@]*? |} Genre.Regex_parser.p_regex in
+  let r =
+    Genre.Parser.parse {|^The end (?:hel?lo world){2,5} ma*na{3,}sd $|}
+      Genre.Regex_parser.p_regex
+    |> Genre.Ast.optimize
+  in
   Stdio.print_s (Genre.Ast.sexp_of_regex r);
   ()
--- lib/ast.ml
+++ lib/ast.ml
@@ -41,6 +41,7 @@ type match_itm =
   | MCharClass of char_class
   | MCharGroup of char_group (* | MCharClassFromUnicodeCategory *)
   | MChar of char
+  | MStr of string
 [@@deriving sexp]
 
 type match_typ = match_itm * quantifier option [@@deriving sexp]
@@ -55,9 +56,30 @@ and subexpression =
   | Group of group
   | Anchor of anchor
   | Backreference of int
-  | Match of match_typ (* | Subexpression of subexpression *)
+  | Match of match_typ
 [@@deriving sexp]
 
 and group = bool * expression * quantifier option [@@deriving sexp]
 
 type regex = bool * expression [@@deriving sexp]
+
+let rec optimize_chars_exp = function
+  | Node (se_l, e) -> Node (optimize_chars_subexp se_l, optimize_chars_exp e)
+  | Leaf se_l -> Leaf (optimize_chars_subexp se_l)
+
+and optimize_chars_subexp (sub_exps : subexpression list) : subexpression list =
+  match sub_exps with
+  | Match (MChar c1, None) :: Match (MChar c2, None) :: rst ->
+      let s = Char.(to_string c1 ^ to_string c2) in
+      Match (MStr s, None) :: rst |> optimize_chars_subexp
+  | Match (MStr s, None) :: Match (MChar c, None) :: rst ->
+      let s' = s ^ Char.to_string c in
+      Match (MStr s', None) :: rst |> optimize_chars_subexp
+  | Group (non_cap, e, q) :: rst ->
+      Group (non_cap, optimize_chars_exp e, q) :: optimize_chars_subexp rst
+  | s :: rst -> s :: optimize_chars_subexp rst
+  | [] -> []
+
+let optimize (rg : regex) : regex =
+  let a, e = rg in
+  (a, optimize_chars_exp e)
